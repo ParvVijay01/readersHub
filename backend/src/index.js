9e52d2804c4e2107587eb93c6dcba4feeb77a1e1
ParvVijay01 <parvvj2000@gmail.com>
2025-05-20 11:58:01 +0530

    cron added

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,13 @@ import authRoutes from "./routes/authRoutes.js"
 import bookRoutes from "./routes/bookRoutes.js"
 import { connectDB } from "./lib/db.js";
 import cors from "cors"
+import job from "./lib/cron.js"
 
 const app = express();
 const PORT = process.env.PORT || 3000
 
 //middleware
+job.start()
 app.use(express.json())
 app.use(cors())
 
